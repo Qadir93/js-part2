39db01ebaf736a309227057a282c747ce3ce3aa5
Qadir93 <abqadirkhan3@gmail.com>
2025-04-10 09:47:49 +0500

    Add files via upload

---
+++ js part2/script.js
@@ -0,0 +1,43 @@
+alert("First Name");
+alert("Last Name");
+alert("Email");
+alert("Phone Number");
+alert("Password");
+
+
+let userAge = 25;
+
+
+let city;
+city = "Pakistan";
+
+
+let teamName = "Jawan Pakistan";
+alert(teamName);
+
+
+var bestMan = "A Hasseb";
+bestMan = "A Qadir";
+
+let caseQty;
+caseQty = 144;
+
+var num = "9"; 
+var num = 9;
+
+let total;
+total = 5 + 10;
+
+
+var merchTotal = 100;
+var shippingCharge = 10;
+var orderTotal = merchTotal + shippingCharge;
+console.log(orderTotal);
+
+
+let score = 20;    
+score = score + 15;  
+
+
+
+
